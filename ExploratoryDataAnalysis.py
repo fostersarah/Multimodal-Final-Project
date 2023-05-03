8e46d2da05d13577f932e90eeeafc943e09c5326
fostersarah <fostersarah@ufl.edu>
2023-05-03 11:20:44 -0400

    Performed exploratory analysis

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import statistics
+import matplotlib.pyplot as plt
+import operator as op
+from scipy.stats.stats import pearsonr
 
 
 def get_statistics(data):
@@ -26,7 +29,6 @@ rpe_data = hr['RPE']
 phys_data = phys_data[:76]
 rpe_data = rpe_data[:76]
 
-
 # Statistics
 phys_statistics = get_statistics(phys_data)
 rpe_statistics = get_statistics(rpe_data)
@@ -37,3 +39,26 @@ data = {'Physiological Data': phys_statistics,
 df = pd.DataFrame(data)
 df.index = ['Mean', 'Median', 'Mode', 'Standard Deviation', 'Variance']
 print(df)
+
+# Data visualization
+
+# plot RPE
+RPE_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+RPE_counts = [op.countOf(rpe_data, 1), op.countOf(rpe_data, 2), op.countOf(rpe_data, 3), op.countOf(rpe_data, 4),
+              op.countOf(rpe_data, 5), op.countOf(rpe_data, 6), op.countOf(rpe_data, 7), op.countOf(rpe_data, 8),
+              op.countOf(rpe_data, 9), op.countOf(rpe_data, 10)]
+
+plt.bar(RPE_labels, RPE_counts)
+plt.title('RPE Distribution')
+plt.xlabel('Rating of Perceived Exertion')
+plt.ylabel('Count')
+plt.show()
+
+# Correlation between RPE and phys data
+plt.scatter(phys_data, rpe_data)
+plt.title('RPE vs Adjusted Heart Rate')
+plt.xlabel('Adjusted Heart Rate')
+plt.ylabel('Rating of Perceived Exertion')
+plt.show()
+
+print(pearsonr(phys_data, rpe_data))
