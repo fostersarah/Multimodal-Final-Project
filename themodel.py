c05a9f6237e73c054d86556ecc19cbf0fb728cb6
Meryem Yuksel <myuksel@ufl.edu>
2023-05-03 16:34:59 -0400

    combinging AU1, AU2, and hr data into one list

--- themodel.py
+++ themodel.py
@@ -9,12 +9,22 @@ from sklearn.tree import DecisionTreeClassifier
 AU1file = "VideoFeatures/AU1.csv"
 AU2file = "VideoFeatures/AU2.csv"
 heartRatefile = "PhysiologicalFeatures/physiologicalData.csv"
+RPE= pd.read_csv("Aw-Soma Data.csv")
+RPE = RPE['RPE'][:76] 
 
 AU1 = pd.read_csv(AU1file);
 AU2 = pd.read_csv(AU2file);
 HR = pd.read_csv(heartRatefile);
 
-print(AU1);
-print(AU2)
-print(HR)
-#looks like all the data is given in the index for the participant
+
+
+XData= []
+
+for i in range(0,75):
+    XData.append([AU1.iloc[i],AU2.iloc[i],HR.iloc[i]])
+
+
+YData = RPE
+
+print(XData[i])
+print(YData)
